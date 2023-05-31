2bbe1eeab562722dcce8dd3487e3c6351cfa27f1
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-31 17:55:58 +0700

    menamabahkan klass seseorang, set akses ke public terdiri dari 2 member fungsi virtual void pesan dan virtual void cetak

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -13,34 +13,3 @@ public:
 	}
 };
 
-class joko : public seseorang {
-public:
-	void pesan() {
-		cout << "pesan dari joko" << endl;
-	}
-	void cetak() {
-		cout << "tulisan joko" << endl;
-	}
-};
-
-class lia : public seseorang {
-public:
-	void pesan() {
-		cout << "pesan dari lia" << endl;
-	}
-};
-
-int main() {
-	seseorang* orang;
-	joko a;
-	lia b;
-
-	orang = &a;
-	orang->pesan();
-	orang = &b;
-	orang->pesan();
-	//a.seseorang::pesan();   memanggil method yang berada di class orang tua
-	//b.seseorang::pesan();
-
-	return 0;
-}
