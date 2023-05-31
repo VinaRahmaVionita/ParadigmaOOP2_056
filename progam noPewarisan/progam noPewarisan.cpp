b221148be6e0fe39fb071a3baf0875b089b339c1
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-31 17:38:45 +0700

    membuat klass orang, set ke public, klass orang berisi umur dan cout untuk mencetak pesan orang di buat dengan umur

---
+++ progam noPewarisan/progam noPewarisan.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final {
+public:
+	virtual void perkenalan() {
+		cout << "hallo saya function dari base class";
+	}
+};
+class derivedClass :public baseClass {
+public:
+	void perkenalan() {
+		cout << "hallo saya function dari base class";
+	}
+};
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
