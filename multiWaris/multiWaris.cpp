672394130bc64e49bdf6f479d7b9ec71fbf7b6f6
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-31 18:30:02 +0700

    menambahkan klass orang , set akses ke publik dan menambhkan kontruktor orang yg berisi nama dan cout unutk mrncetak pesan orang dibuat dan destruktor orang untuk menampilkan sebelum di hapus dan cout untuk mencetak orang dihapus

--- multiWaris/multiWaris.cpp
+++ multiWaris/multiWaris.cpp
@@ -14,38 +14,3 @@ public:
 		cout << "orang dihapus\n" << endl;
 	}
 };
-class manusia {
-public:
-	string jenisKelamin;
-
-	manusia(string pjenisKelamin) {
-		cout << "manusia dibuat\n" << endl;
-	}
-	~manusia() {
-		cout << "manusia dihapus\n" << endl;
-	}	
-};
-
-class pelajar : private manusia, public orang {
-public:
-	string sekolah;
-
-	pelajar(string pNama, string pjenisKelamin, string pSekolah) :
-		orang(pNama),
-		manusia(pjenisKelamin),
-		sekolah(pSekolah) {
-		cout << "pelajar dibuat\n" << endl;
-	}
-	~pelajar() {
-		cout << "pelajar dihapus\n" << endl;
-	}
-	string perkenalan() {
-		return "hallo, nama saya" + nama + "dengan jenis kelamin" + jenisKelamin + "dari sekolah" + sekolah + "\n\n";
-	}
-};
-int main() {
-	pelajar siswa("andi laksono", "laki-laki", "SMAN 1 Bantul");
-	cout << siswa.perkenalan();
-
-	return 0;
-}
