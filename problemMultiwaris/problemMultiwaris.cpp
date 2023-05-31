755f2df32f3274cd79d236a7907f1a8064364ce8
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-31 11:17:19 +0700

    menambahkan kelas budi yang merupakan turunan dari kelas pekerja dan kelas pelajar, set akses ke public, kelas budi memiliki 3 memeber, pUmur merupakan turunan dari pekerja dan pelajar kemudian cout unutk mencetak pesan budi di buat

--- problemMultiwaris/problemMultiwaris.cpp
+++ problemMultiwaris/problemMultiwaris.cpp
@@ -29,3 +29,13 @@ public:
 	}
 };
 
+class budi : public pekerja, public pelajar {
+public:
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi di buat\n" << endl;
+	}
+};
+
